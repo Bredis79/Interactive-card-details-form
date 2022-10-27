c3a378516009acc5e64d299f54b60d1e63fec08b
Bredis <77401763+Bredis79@users.noreply.github.com>
2022-10-28 01:37:12 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ let inputCvc = document.querySelector("#cvc")
 let cardholderSent = document.querySelector(".cardholder--sent")
 const continueBtn = document.querySelector(".cardholder__sent--btn")
 
+
 /*** OUTPUTS ***/
 let cardNumberFront = document.querySelector(".card__front h1")
 let cardHolder = document.querySelector(".card__holder-info p")
@@ -20,34 +21,67 @@ let expYear = document.querySelector("#expY")
 const btnConfirm = document.querySelector(".cardholder--submit")
 
 /*** ERROR BLOCKS***/
-let cardNumberError = document.querySelector(".cardholder--error")
+let cardNameError = document.querySelector(".cardholder__cardName--error")
+let cardNumberError = document.querySelector(".cardholder__cardNumber--error")
 let cardDateError = document.querySelector(".cardholder__date--error")
 let cardCvcError = document.querySelector(".cardholder__cvc--error")
+
+/*** OTHER ***/
+const regex = /[a-zA-Z]/g;
+
+
 
 btnConfirm.addEventListener("click", function(event){
     event.preventDefault()
 
-// write name on front side
-cardHolder.textContent = event.path[1][0].value
+// write name on front side...Wnen is empty let current value
+if(event.path[1][0].value == ""){
+  
+  cardHolder.textContent = cardHolder.textContent
+
+  // show error block
+  cardNameError.style.display = "block"
+
+  // add error border
+  inputName.classList.add("inputError")
+
+} else {
+  cardHolder.textContent = event.path[1][0].value
+
+  // hide error block
+  cardNameError.style.display = "none"
+
+  // remove error border
+  inputName.classList.remove("inputError")
+}
 
 
-// select card number from input
-cardNumberFront.textContent = event.path[1][1].value
+// control card number value from input. Number has to be min 16 char. long
+if(event.path[1][1].value == "" || event.path[1][1].value.length < 16){
 
+
+
+  // if input is empty or has less than 16 char. then show "default" value
+  cardNumberFront.textContent = cardNumberFront.textContent
+} else {
+
+  // else return the input value
+  cardNumberFront.textContent = event.path[1][1].value
+}
     // slice number on 4 parts and set each as a number
     let wholeNumber = cardNumberFront.textContent
-      let number1 = Math.floor(wholeNumber.slice(0, 4))
-      let number2 = Math.floor(wholeNumber.slice(4, 8))
-      let number3 = Math.floor(wholeNumber.slice(8, 12))
-      let number4 = Math.floor(wholeNumber.slice(12, 16))
+      let number1 = wholeNumber.slice(0, 4)
+      let number2 = wholeNumber.slice(4, 8)
+      let number3 = wholeNumber.slice(8, 12)
+      let number4 = wholeNumber.slice(12, 16)
 
     // connect numbers together with whitespace  
     let numberOutput = `${number1} ${number2} ${number3} ${number4}`
-    
+   
+
         // write number on front side of card
-        if(event.path[1][1].value.length <= 0){
-          cardNumberFront.textContent = `0000 0000 0000 0000`   // if input is empty
-         
+        if(event.path[1][1].value.length <= 0 || event.path[1][1].value.length < 16 || event.path[1][1].value.match(regex)){
+                   
           // add error border  
           inputCardNumber.classList.add("inputError")
           
@@ -55,86 +89,89 @@ cardNumberFront.textContent = event.path[1][1].value
           cardNumberError.style.display = "block"
         
         } else if (event.path[1][1].value.length > 0){
-        cardNumberFront.textContent = numberOutput
-
-        // remove error border  
-        inputCardNumber.classList.remove("inputError")
           
-        // hide error block
-        cardNumberError.style.display = "none"
+          cardNumberFront.textContent = numberOutput
+
+          // remove error border  
+          inputCardNumber.classList.remove("inputError")
+          
+          // hide error block
+          cardNumberError.style.display = "none"
       
         }
-        
-//get day from input and put it on the card
+           
 
 
- if (event.path[1][2].value > 0 && event.path[1][2].value < 13 ) {
+      
+
+//get month from input and put it on the card
+ if (event.path[1][2].value > 0 && event.path[1][2].value <= 12 ) {
     expMonth.textContent = event.path[1][2].value 
-    // remove error border
+   
+     // remove error border
      inputMonth.classList.remove("inputError") 
      
      // hide error block
      cardDateError.style.display = "none"
-     
-     // when is one digit put 0 before
-      if(event.path[1][2].value.length < 2){
-        expMonth.textContent = `0${event.path[1][2].value}`
-      }
+  
+ } else if(event.path[1][2].value == 0 || event.path[1][2].value > 12){
 
-
- } else {
+    // show error block
     cardDateError.style.display = "block"
    
     // add error border
     inputMonth.classList.add("inputError") 
 
-    // hide error block
-    cardDateError.style.display = "none"
+    expMonth.textContent = "00"
+
 }
 
-
+// when is one digit put 0 before
+      if(event.path[1][2].value.length < 2){
+        expMonth.textContent = `0${event.path[1][2].value}`
+      }
 
 // get year from input and put it on the card
-
 if (event.path[1][3].value.length > 0 && event.path[1][3].value > 22 && event.path[1][3].value < 40) {
     expYear.textContent = event.path[1][3].value
-   
+    
+    
     // remove error border
     inputYear.classList.remove("inputError") 
     
 } else {
+    // show error block
     cardDateError.style.display = "block"
     
     // add error border
     inputYear.classList.add("inputError") 
-
+  
+    
 }
   
-// get value from input and change number on back side of card
-if(event.path[1][4].value.length > 0) {
+// get value from input and change number on back side of card...number has to have 3 digits
+if(event.path[1][4].value.length == 3) {
     cardBack[0].textContent = event.path[1][4].value
    
-    // value has to have 3 numbers
-    if(event.path[1][4].value.length == 3) {
+   // hide error block
+   cardCvcError.style.display = "none"
 
-      // hide error block  
-      cardCvcError.style.display = "none"
-
-      // remove error border
-      inputCvc.classList.remove("inputError")
-    }
- } else {
-
-    // show error block
-    cardCvcError.style.display = "block"
-
-    // add error border
-    inputCvc.classList.add("inputError")
- }
+   // remove error border
+   inputCvc.classList.remove("inputError")
 
 
-if(inputCardNumber.value == "" || inputMonth.value == "" || inputYear.value == "" || inputCvc.value == ""){
-  console.log("Something went wrong")
+  } else {
+
+      // show error block
+      cardCvcError.style.display = "block"
+
+      // add error border
+      inputCvc.classList.add("inputError")
+  }
+
+// control if unputs are empty
+if(inputName.value == "" || inputCardNumber.value == "" || inputMonth.value == "" || inputYear.value == "" || inputCvc.value == ""){
+  console.log("Some inputs are empty")
 } else {
   // hide form inputs 
 inputsCardholder.style.display = "none"
@@ -153,7 +190,7 @@ continueBtn.addEventListener("click", function(event){
   // hide thank you messege
   cardholderSent.style.display = "none"
 
-  
+  // reset all values
   location.reload();
  
 
